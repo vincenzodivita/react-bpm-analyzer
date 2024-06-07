9d06238b07d84f2049d6adaa78e9fa916356a109
vincenzodivita <divitavincenzo@icloud.com>
2024-06-07 13:08:26 +0200

    Import FileUploader.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import BpmAnalyzer from './components/BpmAnalyzer';
+import FileUploader from './components/FileUploader';
 
 function App() {
+  const [file, setFile] = useState(null);
+
   return (
     <div className="App">
-      <BpmAnalyzer />
+      <h1>Analizzatore di BPM</h1>
+      <FileUploader onFileSelect={setFile} />
+      <BpmAnalyzer file={file} />
     </div>
   );
 }
